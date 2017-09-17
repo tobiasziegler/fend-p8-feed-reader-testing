1c5fb30e2d33957e57154e1bf5e112c201f35ca4
David Mallard <correspondence@davidmallard.id.au>
2017-09-17 16:36:10 +1000

    Write the test to ensure each feed has a non-empty name

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -39,10 +39,16 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* DONE: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have names', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
+        });
     });
 
 
